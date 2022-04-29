5cd85064bae5c79ffc786f2c498a2511cd9eae07
srayo16 <srayosikder@gmail.com>
2022-04-29 20:40:38 +0600

    main api setted

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ app.use(cors());
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pazji.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('db connected');
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("inventory").collection("stocks");
+
+        app.get('/inventory' , async (req , res)=>{
+            const query = {};
+            const cursor = database.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+     
+    } finally {
+        
+    }
+}
+run().catch(console.dir);
+
+
 
 
 
